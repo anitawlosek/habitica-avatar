94aed50579e5e0f5cf8333cf597f485e481277f9
Anita Włosek <anita.wlosek@gmail.com>
2025-09-22 18:58:49 +0000

    build project with vite

---
+++ index.ts
@@ -0,0 +1,2 @@
+export { default as HabiticaAvatar } from './HabiticaAvatar';
+export type { User } from './types/User';
---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: './index.ts',
+      name: 'HabiticaAvatar',
+      fileName: 'index',
+      formats: ['es', 'cjs'],
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
+});
