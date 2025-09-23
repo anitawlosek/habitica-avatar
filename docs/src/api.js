5988db3bcddde652674922d8cc2274cff3e3c6fc
Anita Włosek <anita.wlosek@gmail.com>
2025-09-23 20:25:25 +0000

    removed unused files

--- docs/src/api.js
+++
@@ -1,5 +0,0 @@
-'use strict'
-
-var Habitica = require('habitica')
-
-module.exports = new Habitica()
--- docs/src/get-value.js
+++
@@ -1,5 +0,0 @@
-'use strict'
-
-module.exports = function getValue (name) {
-  return document.querySelector('#' + name).value
-}
